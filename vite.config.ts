06b1285117359b6828a45cb23a49b61f16d356cf
Jan Dalheimer <jan.dalheimer@sweco.se>
2026-01-26 12:49:39 +0100

    infra: auto-deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,36 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
+const isPagesBuild = process.env.VITE_DEPLOY_TARGET === "pages";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      fileName: "index",
-      entry: resolve(__dirname, "lib/index.ts"),
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "@mantine/core"],
-    },
-  },
-});
+export default defineConfig(
+  isPagesBuild
+    ? {
+        base: "/sweco-theme-mantine/",
+        plugins: [react()],
+        build: {
+          outDir: "dist-pages",
+          emptyOutDir: true,
+        },
+      }
+    : {
+        plugins: [react(), dts({ include: ["lib"] })],
+        build: {
+          copyPublicDir: false,
+          lib: {
+            fileName: "index",
+            entry: resolve(__dirname, "lib/index.ts"),
+            formats: ["es", "cjs"],
+          },
+          rollupOptions: {
+            external: [
+              "react",
+              "react-dom",
+              "react/jsx-runtime",
+              "@mantine/core",
+            ],
+          },
+        },
+      },
+);
