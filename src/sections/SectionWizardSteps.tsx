e165be2343e910d71b2945f4119e54f08caf172f
Elsa Hedström <elsa.hedstrom@sweco.se>
2026-02-19 08:43:28 +0100

    update stepper based on sweco design system

--- src/sections/SectionWizardSteps.tsx
+++ src/sections/SectionWizardSteps.tsx
@@ -6,10 +6,10 @@ export const SectionWizardSteps: FC = () => (
   <Section title="Wizard steps">
     <Group mt="sm">
       <Stepper active={2} orientation="vertical">
-        <Stepper.Step label="Step 1" />
-        <Stepper.Step label="Step 2" />
-        <Stepper.Step label="Step 3" />
-        <Stepper.Step label="Step 4" />
+        <Stepper.Step label="Step 1" description="Description for step 1" />
+        <Stepper.Step label="Step 2" description="Description for step 2" />
+        <Stepper.Step label="Step 3" description="Description for step 3" />
+        <Stepper.Step label="Step 4" description="Description for step 4" />
       </Stepper>
       <Stepper active={2}>
         <Stepper.Step label="Step 1" />
